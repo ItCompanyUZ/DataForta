fd9e7b15ae36cb8759d2d1817dc4e086f23edfd2
ramziddin1671 <ramziddin_jurabek.uz@mail.ru>
2022-03-02 21:03:47 +0500

    forms chala

---
+++ Dataforta/apps/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.forms import ModelForm
+
+
+from .models import *
+
+
+class ContactForm(ModelForm):
+
+    class Meta:
+        model = Contact
+        fields = ['name', 'theme', 'email', 'text']
--- Dataforta/apps/models.py
+++ Dataforta/apps/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    theme = models.CharField(max_length=200)
+    email = models.EmailField()
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Kelgan Izoxlar'
+        verbose_name_plural = 'Kelgan Izoxlar'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("apps:index", args=[self.id])
